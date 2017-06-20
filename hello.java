1a3b50dfe8365f895fd44b5e31d9a4f59d7c6163
sherlyedward <sherlinhelenjk@gmail.com>
2017-06-20 16:54:55 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class hello
+{
+  public static void main (String[] args) throws java.lang.Exception
+  {
+    int i;
+    for(i=1;i<=5;i++)
+    {
+     System.out.println("Hello");
+    }
+  }
+}
